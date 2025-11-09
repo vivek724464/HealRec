4a75040050a87b9299f079bdbb3f51a1f5ea5282
Vivek Kumar <vivekbxr30@gmail.com>
2025-11-09 19:04:41 +0530

    updated resource-type and format

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -2,26 +2,27 @@ const express = require("express");
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("../config/cloudinary");
-const reportController = require("../controllers/reportController");
+const {uploadReport, getReports}= require("../controllers/reportController");
 
 const router = express.Router();
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: (req, file) => {
-    const patientId = req.body.patientId || "general";
+    const patientId = req.body.patientId;
     return {
       folder: `healrec_reports/${patientId}`,
-      resource_type: "auto", 
-      allowed_formats: ["jpg", "png", "jpeg", "pdf"],
+       resource_type: "auto",
+      format: file.originalname.split(".").pop(),
+      allowed_formats: ["jpg", "png", "jpeg", "pdf", "docx"],
     };
   },
 });
 
 const upload = multer({ storage });
 
-router.post("/uploadReport", upload.single("report"), reportController.uploadReport);
+router.post("/uploadReport", upload.single("report"), uploadReport);
 
-router.get("/:patientId", reportController.getReports);
+router.get("/:patientId", getReports);
 
 module.exports = router;
